f4f9ad76e4731de1d25eb987fac367982e335dc6
Jan <jan.leisbrock@web.de>
2025-01-13 12:15:33 +0100

    Environment hustle

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,6 +1,22 @@
 from torch.utils.data import Dataset
 
-from renewable_energy_price_prediction.data import MyDataset
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from the .env file
+load_dotenv()
+
+# Get the PYTHONPATH or any custom path from .env
+python_path = os.getenv("PYTHONPATH")
+
+# Add the path to sys.path if it's set
+if python_path and python_path not in os.sys.path:
+    os.sys.path.insert(0, python_path)
+
+
+
+
+from src.renewable_energy_price_prediction.data import MyDataset
 
 
 def test_my_dataset():
