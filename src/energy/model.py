5fe95cd644b989d8d4e57557fca5a28ceb897c53
Jonatan Svendsen <Jhz209@alumni.ku.dk>
2025-01-20 13:02:27 +0100

    model change

--- src/energy/model.py
+++ src/energy/model.py
@@ -4,15 +4,16 @@ from torch import nn
 
 
 class NeuralNetwork(pl.LightningModule):
-    def __init__(self, input_size: int):
-        super(NeuralNetwork, self).__init__()
-        self.fc1 = nn.Linear(input_size, 64)
-        self.fc2 = nn.Linear(64, 32)
+    def __init__(self, input_size: int, hidden_units=64, lr=1e-3):
+        super().__init__()
+        self.save_hyperparameters()
+        self.fc1 = nn.Linear(input_size, hidden_units)
+        self.fc2 = nn.Linear(hidden_units, 32)
         self.fc3 = nn.Linear(32, 1)
         self.relu = nn.ReLU()
-
-        # Assuming regression task; change loss if classification or other tasks
         self.loss_fn = nn.MSELoss()
+        self.lr = lr
+
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.relu(self.fc1(x))
